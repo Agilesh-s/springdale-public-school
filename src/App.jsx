efb6941c43d8d1d81ddb1194ab60b81ad0e767c4
Agilesh07 <agileshwins@gmail.com>
2024-07-26 13:39:30 +0530

    Created AboutUs section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home'
 import Navbar from './components/Navbar'
+import About from './components/About';
 
 function App() {
   return (
     <>
       <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </main>
+      <Home />
+      <About />
     </>
   )
 }
